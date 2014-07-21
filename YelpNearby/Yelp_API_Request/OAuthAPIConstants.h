104d4b3104b1d99182a728448f32000f3fd026d4
Jesus Magana <maganazero5@gmail.com>
2014-07-20 21:42:36 -0700

    changed logic

--- YelpNearby/Yelp_API_Request/OAuthAPIConstants.h
+++ YelpNearby/Yelp_API_Request/OAuthAPIConstants.h
@@ -9,10 +9,10 @@
 #ifndef YelpNearby_OAuthAPIConstants_h
 #define YelpNearby_OAuthAPIConstants_h
 
-#define OAUTH_CONSUMER_KEY @"9Ws4fZ1G10Nc2a4ryI-_ag"
-#define OAUTH_CONSUMER_SECRET @"z1YRlvNDbjCSiLpOj9yr6AoUTrE"
-#define OAUTH_TOKEN @"Qq_3txE-2cl3_p5KP6I9nm4ubcS4Y21F"
-#define OAUTH_TOKEN_SECRET @"q4ZgOTQ2VbGFFxHvEFz5VoMXNpM"
+#define OAUTH_CONSUMER_KEY @"-_ag"
+#define OAUTH_CONSUMER_SECRET @""
+#define OAUTH_TOKEN @"Qq_3txE-"
+#define OAUTH_TOKEN_SECRET @""
 #define YELP_SEARCH_URL @"http://api.yelp.com/v2/search"
 
 #endif
